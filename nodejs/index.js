d40498f29b2c3dfd2efc37fc489dac9b4715cc3c
jurikolo <jurikolo@yandex.com>
2015-10-20 22:13:18 +0300

    Replaced yum with dnf, replaces index with redis autoincrement

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,28 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http'),
+    redis = require('redis');
 
-// Constants
-var PORT = 8080;
-
-// App
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World\n');
+
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+
+// APPROACH 1: Using environment variables created by Docker
+// var client = redis.createClient(
+// 	process.env.REDIS_PORT_6379_TCP_PORT,
+//   	process.env.REDIS_PORT_6379_TCP_ADDR
+// );
+
+// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+var client = redis.createClient('6379', 'redis');
+
+
+app.get('/', function(req, res, next) {
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    res.send('This page has been viewed ' + counter + ' times!');
+  });
 });
 
-app.listen(PORT)
-console.log('Running on http://localhost:' + PORT);
+http.createServer(app).listen(process.env.PORT || 8080, function() {
+  console.log('Listening on port ' + (process.env.PORT || 8080));
+});
